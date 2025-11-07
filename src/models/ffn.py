7afb9bc8cce9fffe007849c9d3e7d0a7bd687902
wuyanzu-song <1726296417@qq.com>
2025-11-07 14:28:45 +0800

    Update ffn.py

--- src/models/ffn.py
+++ src/models/ffn.py
@@ -2,9 +2,6 @@ import torch.nn as nn
 
 
 class PositionwiseFFN(nn.Module):
-    """
-    Position-wise Feed-Forward Network
-    """
 
     def __init__(self, d_model, d_ff, dropout=0.1):
         super().__init__()
@@ -14,11 +11,5 @@ class PositionwiseFFN(nn.Module):
         self.activation = nn.GELU()
 
     def forward(self, x):
-        """
-        Args:
-            x: [batch_size, seq_len, d_model]
 
-        Returns:
-            output: [batch_size, seq_len, d_model]
-        """
         return self.linear2(self.dropout(self.activation(self.linear1(x))))
