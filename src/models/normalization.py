7d1c963e357ced0b49ca13886a7091fc5ca73446
wuyanzu-song <1726296417@qq.com>
2025-11-07 14:30:18 +0800

    Update normalization.py

--- src/models/normalization.py
+++ src/models/normalization.py
@@ -3,9 +3,6 @@ from sympy.printing.pytorch import torch
 
 
 class LayerNorm(nn.Module):
-    """
-    Layer Normalization
-    """
 
     def __init__(self, d_model, eps=1e-12):
         super().__init__()
@@ -14,13 +11,7 @@ class LayerNorm(nn.Module):
         self.eps = eps
 
     def forward(self, x):
-        """
-        Args:
-            x: [batch_size, seq_len, d_model]
 
-        Returns:
-            normalized_x: [batch_size, seq_len, d_model]
-        """
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
 
